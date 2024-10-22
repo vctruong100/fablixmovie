26b07a74a5932d0e8d343bc77fc07e5edee5a7db
VcTruong <vctruong100@gmail.com>
2024-10-21 18:15:25 -0700

    movielist: update servlet to handle browsing and searching
    Added navigator bar to the html page and updated js. (Can't the results
    to generate for some reason)

--- WebContent/movielist.js
+++ WebContent/movielist.js
@@ -9,60 +9,75 @@ function handleMovieResult(resultData) {
     // Find the element to populate the movie list
     let movieListElement = jQuery("#movie_list_body");
 
-    // Iterate through resultData, display up to 20 movies
-    for (let i = 0; i < Math.min(20, resultData.length); i++) {
-        // Create a new row for each movie
+    // Iterate through resultData and populate the movie list
+    resultData.forEach(movie => {
         let rowHTML = "<tr>";
-
-        // Add movie title with a link to the single movie page
-        rowHTML +=
-            "<td><a href='single-movie.html?id=" + resultData[i]['movie_id'] + "'>" +
-            resultData[i]['movie_title'] +
-            "</a></td>";
-
-        rowHTML += "<td>" + resultData[i]['movie_year'] + "</td>";
-        rowHTML += "<td>" + resultData[i]['movie_director'] + "</td>";
-        rowHTML += "<td>" + resultData[i]['movie_rating'] + "</td>";
-        rowHTML += "<td>" + resultData[i]['movie_num_votes'] + "</td>";
+        rowHTML += `<td><a href="single-movie.html?id=${movie.movie_id}">${movie.movie_title}</a></td>`;
+        rowHTML += `<td>${movie.movie_year}</td>`;
+        rowHTML += `<td>${movie.movie_director}</td>`;
+        rowHTML += `<td>${movie.movie_rating}</td>`;
+        rowHTML += `<td>${movie.movie_num_votes}</td>`;
 
         // Genres
         rowHTML += "<td>";
-        let genres = resultData[i]["movie_genres"];
-        for (let j = 0; j < genres.length; j++) {
-            rowHTML += genres[j]["genre_name"];
-            if (j < genres.length - 1) {
-                rowHTML += ", ";
-            }
-        }
+        movie.movie_genres.forEach((genre, index) => {
+            rowHTML += genre.genre_name;
+            if (index < movie.movie_genres.length - 1) rowHTML += ", ";
+        });
         rowHTML += "</td>";
 
-        // Stars (with hyperlinks)
+        // Stars
         rowHTML += "<td>";
-        let stars = resultData[i]["movie_stars"];
-        for (let j = 0; j < stars.length; j++) {
-            rowHTML += "<a href='single-star.html?id=" + stars[j]["star_id"] + "'>" +
-                stars[j]["star_name"] + "</a>";
-            if (j < stars.length - 1) {
-                rowHTML += ", ";
-            }
-        }
+        movie.movie_stars.forEach((star, index) => {
+            rowHTML += `<a href="single-star.html?id=${star.star_id}">${star.star_name}</a>`;
+            if (index < movie.movie_stars.length - 1) rowHTML += ", ";
+        });
         rowHTML += "</td>";
 
         rowHTML += "</tr>";
-
-        // Append the row to the table body
         movieListElement.append(rowHTML);
-    }
+    });
 }
+
+// Extract query parameters from the URL for search or browsing
+function getQueryParams() {
+    const params = new URLSearchParams(window.location.search);
+    let queryString = "";
+
+    if (params.has("title")) {
+        queryString += `&title=${params.get("title")}`;
+    }
+    if (params.has("year")) {
+        queryString += `&year=${params.get("year")}`;
+    }
+    if (params.has("director")) {
+        queryString += `&director=${params.get("director")}`;
+    }
+    if (params.has("star")) {
+        queryString += `&star=${params.get("star")}`;
+    }
+    if (params.has("genre")) {
+        queryString += `&genre=${params.get("genre")}`;
+    }
+    if (params.has("start")) {
+        queryString += `&start=${params.get("start")}`;
+    }
+
+    return queryString;
+}
+
 
 /**
  * Once this .js is loaded, the following scripts will be executed by the browser.
  */
+jQuery(document).ready(() => {
+    const queryParams = getQueryParams();
 
-// Makes the HTTP GET request and registers on success callback function handleMovieResult
-jQuery.ajax({
-    dataType: "json",  // Setting return data type
-    method: "GET",     // Setting request method
-    url: "api/movie-list",  // Setting request URL, which is mapped by MovieListServlet
-    success: (resultData) => handleMovieResult(resultData)  // Setting callback function to handle the returned data
+    // Makes the HTTP GET request and registers on success callback function handleMovieResult
+    jQuery.ajax({
+        dataType: "json",  // Setting return data type
+        method: "GET",     // Setting request method
+        url: `api/movie-list?${queryParams}`,
+        success: (resultData) => handleMovieResult(resultData)  // Setting callback function to handle the returned data
+    });
 });
